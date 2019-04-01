179b16c4e32c5a5f5c7dd896147e37beb140e66b
Bashton19 <35300968+Bashton19@users.noreply.github.com>
2019-04-01 18:23:09 +0100

    Formatting

--- hubble/views.py
+++ hubble/views.py
@@ -15,17 +15,18 @@ class PostListView(ListView):
     template_name = 'hubble/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 10
+    paginate_by = 6
     
 class UserPostListView(ListView):
     model = Post
     template_name = 'hubble/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 6
     
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
+    
     
 class PostDetailView(DetailView):
     model = Post
