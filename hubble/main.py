afddcd447770f717e14f40117268cedba8832aac
Bashton19 <35300968+Bashton19@users.noreply.github.com>
2019-04-10 20:16:51 +0100

    Added main.py containing migration code

---
+++ hubble/main.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+def get_connection():
+    if 'db' not in locals():
+        db = mysql.connector.connect(host="localhost",user="root",passwd="",database="hubble41")
+    return db
+
+def close_connection():
+    if 'db' in locals():
+        database.close()
+
+def user_exists(email, password):
+    database = get_connection()
+    cursor = database.cursor()
+    
+    sql = "SELECT * FROM user_info WHERE Email=%s AND Password=%s"
+    escape = (email, password)
+
+    cursor.execute(sql, escape)
+
+    result = cursor.fetchall()
+
+    return len(result) == 1
+
+print(user_exists("james.kierans99@gmail.com", "james1235"))
