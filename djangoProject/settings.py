5c2b2c7d41e5dc4552922515d6582d1ddd1d9354
Bashton <35300968+Bashton19@users.noreply.github.com>
2019-04-30 14:53:52 +0100

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'heroku_c1eaab334582a09',
-        'HOST': 'cf3ef000@eu-cdbr-west-02.cleardb.net',
+        'HOST': 'eu-cdbr-west-02.cleardb.net',
         'USER': 'b0ac306c278038',
         'PASSWORD': 'cf3ef000',
     }
